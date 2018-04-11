53114882f703384acf8898ca68649a2fb0092394
黃聖雄 <evalhero@gmail.com>
2018-04-11 13:45:44 +0800

    opencv340

---
+++ code/opencv340/include/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/opencv340/sources/include
+# Build directory: D:/opencv340/build/include
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
